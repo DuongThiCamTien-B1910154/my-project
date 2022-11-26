156aeef0bd0cb0b954bb06126bba1f280bc9c405
duongthucamtien123 <tienb1910154@student.ctu.edu.vn>
2022-11-27 00:07:26 +0700

    ptweb

--- server/app.js
+++ server/app.js
@@ -27,13 +27,18 @@ mongoose
 
 // routes prefix
 
-app.use("/api/product",require("./routes/product.route"));
+app.use("/api/product", require("./routes/product.route"));
 
-app.use("/api/category",require("./routes/category.route"));
+app.use("/api/category", require("./routes/category.route"));
 
-app.use("/api/user",require("./routes/user.route"));
+app.use("/api/user", require("./routes/user.route"));
 
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname + "/dist/"));
+  app.get("*", (res, req) => {
+    res.sendFile(__dirname + "/dist/index.html");
+  });
+}
 // start server
 app.listen(port, () =>
   console.log(`server running at http://localhost:${port}`)
